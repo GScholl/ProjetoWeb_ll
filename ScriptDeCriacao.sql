6bca24cb4095d56eeaf531b2e710cf5c9b3b5c37
Guilherme Scholl <108899269+GScholl@users.noreply.github.com>
2023-10-25 13:56:49 -0300

    Script de criação do banco de dados

--- ScriptDeCriacao.sql
+++ ScriptDeCriacao.sql
@@ -1,19 +1,28 @@
+
+CREATE DATABASE projeto_web2;
+use projeto_web2;
 CREATE TABLE categoria(
-
-
-
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(64),
+    PRIMARY KEY(id)
 );
-
 
 CREATE TABLE produto(
-
-
+    id INT NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(1024) NOT NULL,
+    descricao TEXT NOT NULL,
+    id_categoria INT NOT NULL,
+    valor DECIMAL(10, 2),
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id)
 );
 
-CREATE TABLE venda(
+CREATE TABLE fotos_produto(
+    id INT NOT NULL AUTO_INCREMENT,
+    url TEXT NOT NULL,
+    alt VARCHAR(64),
+    id_produto INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_produto) REFERENCES produto(id)
+);
 
-
-
-
-    
-)
